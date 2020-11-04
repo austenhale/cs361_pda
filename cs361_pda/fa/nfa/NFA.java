f655263a2b06b12c17653ce65cf46c73e03f8c3c
auste <auste@10.101.153.1>
2020-11-04 10:09:15 -0700

    Adding NFA and NFAState

---
+++ cs361_pda/fa/nfa/NFA.java
@@ -0,0 +1,63 @@
+package fa.nfa;
+
+import java.util.Set;
+
+import fa.State;
+import fa.dfa.DFA;
+
+public class NFA implements fa.FAInterface{
+
+	@Override
+	public void addStartState(String name) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void addState(String name) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void addFinalState(String name) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void addTransition(String fromState, char onSymb, String toState) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public Set<? extends State> getStates() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Set<? extends State> getFinalStates() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public State getStartState() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Set<Character> getABC() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public DFA getDFA() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
