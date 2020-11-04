e326500c4cdfea0c98b3508d76735db34095311c
auste <auste@10.101.153.1>
2020-11-04 10:31:28 -0700

    Implementation of most of NFA[State]

--- cs361_pda/fa/nfa/NFA.java
+++ cs361_pda/fa/nfa/NFA.java
@@ -1,60 +1,157 @@
 package fa.nfa;
 
+import java.util.LinkedHashSet;
 import java.util.Set;
 
 import fa.State;
 import fa.dfa.DFA;
+import fa.dfa.DFAState;
 
 public class NFA implements fa.FAInterface{
-
-	@Override
-	public void addStartState(String name) {
-		// TODO Auto-generated method stub
-		
+	
+	private Set<NFAState> states;
+	private NFAState start;
+	private Set<Character> language;
+	
+	public NFA() {
+		states = new LinkedHashSet<NFAState>();
+		language = new LinkedHashSet<Character>();
 	}
 
 	@Override
+	public void addStartState(String name) {
+		NFAState s = checkIfExists(name);
+		if (s == null) {
+			s = new NFAState(name);
+			states.add(s);
+		}else {
+			System.out.println("WARNING: A state with name " + name + " already exists in the NFA");
+		}
+		start = s;
+		
+	}
+
+	
+
+	@Override
 	public void addState(String name) {
-		// TODO Auto-generated method stub
+		NFAState s = checkIfExists(name);
+		if (s == null) {
+			s = new NFAState(name);
+			states.add(s);
+		}else {
+			System.out.println("WARNING: A state with name " + name + " already exists in the NFA");
+		}
 		
 	}
 
 	@Override
 	public void addFinalState(String name) {
-		// TODO Auto-generated method stub
+		NFAState s = checkIfExists(name);
+		if (s == null) {
+			s = new NFAState(name, true);
+			states.add(s);
+		}else{
+			System.out.println("WARNING: A state with name " + name + " already exists in the NFA");
+		}
 		
 	}
 
 	@Override
 	public void addTransition(String fromState, char onSymb, String toState) {
-		// TODO Auto-generated method stub
+		NFAState from = checkIfExists(fromState);
+		NFAState to = checkIfExists(toState);
+		if (from == null) {
+			System.err.println("ERROR: No NFA state exists with name " + fromState);
+			System.exit(2);
+		}else if (to == null) {
+			System.err.println("ERROR: NO NFA state exists with name " + toState);
+			System.exit(2);
+		}
+		from.addTransition(onSymb, to);
+		
+		if (!language.contains(onSymb)) {
+			language.add(onSymb);
+		}
 		
 	}
 
 	@Override
 	public Set<? extends State> getStates() {
-		// TODO Auto-generated method stub
-		return null;
+		return states;
 	}
 
 	@Override
 	public Set<? extends State> getFinalStates() {
-		// TODO Auto-generated method stub
-		return null;
+		Set<NFAState> ret = new LinkedHashSet<NFAState>();
+		for (NFAState s : states) {
+			if (s.isFinal()) {
+				ret.add(s);
+			}
+		}
+		return ret;
 	}
 
 	@Override
 	public State getStartState() {
-		// TODO Auto-generated method stub
-		return null;
+		return start;
 	}
 
 	@Override
 	public Set<Character> getABC() {
-		// TODO Auto-generated method stub
-		return null;
+		return language;
 	}
 
+	
+	private NFAState checkIfExists(String name) {
+		NFAState ret = null;
+		for (NFAState s : states) {
+			if (s.getName().equals(name)) {
+				ret = s;
+				break;
+			}
+		}
+		return ret;
+	}
+	
+	@Override
+	public String toString(){
+
+		String s = "Q = { ";
+		String fStates = "F = { ";
+		for(NFAState state : states){
+			s += state.toString();
+			s +=" ";
+			if(state.isFinal()){
+				fStates +=state.toString();
+				fStates += " ";
+			}
+		}
+		s += "}\n";
+		fStates += "}\n";
+		s += "Sigma = { ";
+		for(char c : language){
+			s += c + " ";
+		}
+		s += "}\n";
+		//create transition table
+		s += "delta =\n"+String.format("%10s", "");;
+		for(char c : language){
+			s += String.format("%10s", c);
+		}
+		s+="\n";
+		for(NFAState state : states){
+			s += String.format("%10s",state.toString());
+			for(char c : language){
+				s += String.format("%10s", state.getTo(c).toString());
+			}
+			s+="\n";
+		}
+		//start state
+		s += "q0 = " + start + "\n";
+		s += fStates;
+		return s;
+	}
 	public DFA getDFA() {
 		// TODO Auto-generated method stub
 		return null;
--- cs361_pda/fa/nfa/NFAState.java
+++ cs361_pda/fa/nfa/NFAState.java
@@ -1,5 +1,48 @@
 package fa.nfa;
 
+import java.util.HashMap;
+
 public class NFAState extends fa.State{
+	
+	private HashMap<Character, NFAState> delta;
+	private boolean isFinal;
+	
+
+	public NFAState(String name) {
+		initDefault(name);
+		isFinal = false;
+	}
+
+	
+	public NFAState(String name, boolean b) {
+		initDefault(name);
+		this.isFinal = b;
+	}
+	
+	private void initDefault(String name) {
+		this.name = name;
+		delta = new HashMap<Character, NFAState>();
+		
+	}
+	
+	public boolean isFinal() {
+		return isFinal;
+	}
+
+
+	public void addTransition(char onSymb, NFAState toState) {
+		delta.put(onSymb, toState);
+		
+	}
+
+
+	public Object getTo(char symb) {
+		NFAState ret = delta.get(symb);
+		if (ret==null) {
+			System.err.println("ERROR: NFAState.getTo(char symb) returns null on " + symb + " from " + name);
+			 System.exit(2);
+		}
+		return delta.get(symb);
+	}
 
 }
